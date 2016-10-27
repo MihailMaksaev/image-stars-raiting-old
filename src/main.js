7383ad627a429355da969873046f4148e6d076af
Mihail <mihailmaksaev@rambler.ru>
2016-10-28 00:52:18 +0400

    osnova js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,49 @@
 import './main.css' 
 
-console.log("7");
+console.log("11");
+
+var starSize = 32;
+
+
+var controlStars = document.getElementById("control_stars");
+
+var controlStarsOffset = controlStars.offsetLeft;
+
+var scrollStars =  controlStars.querySelector("div");
+
+scrollStars.style.width="0px";
+
+controlStars.addEventListener("click", clickOnStar);
+
+controlStars.addEventListener("mouseover", hoverOnStar);
+
+controlStars.addEventListener("mouseout", hoverOutOnStar);
+
+
+function hoverOnStar(e){
+	
+    var clickCoord = e.pageX - controlStarsOffset;
+	
+	scrollStars.style.width = clickCoord+"px";
+	
+}
+
+function hoverOutOnStar(e){
+	
+	
+	scrollStars.style.width = "0px";
+	
+}
+
+function clickOnStar(e){
+	
+	
+	var clickCoord = e.pageX - controlStarsOffset;
+	
+	scrollStars.style.width = clickCoord+"px";
+	
+	
+	console.dir(e.pageX);
+	console.dir(clickCoord);
+	
+	}
