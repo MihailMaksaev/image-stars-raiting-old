545c205652a5c957d4970c07cbc0f424e8ef3e0e
Mihail <mihailmaksaev@rambler.ru>
2016-10-27 23:25:30 +0400

    sborka 27/10/2016

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
-console.log("1");
+import './main.css' 
+
+console.log("7");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -18,5 +20,33 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin()
-  ]
+  ],
+  module: {  
+    preLoaders: [ 
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
+    loaders: [
+      {
+        loader: 'babel?presets[]=es2015',
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        test: /\.js$/
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+      },
+	  { test: /\.png$/,    loader: "url-loader?limit=10000&minetype=image/png" }
+    ]
+  },
+    postcss: function () {
+    return [autoprefixer, precss];
+  }
 }
